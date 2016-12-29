5fe2e5adf92d7b60e22721facab15d44b5dbc086
luisdelgado <lhds@cin.ufpe.br>
2016-12-29 08:25:00 -0300

    Iluminação especular

--- PathTraceIntegrator.py
+++ PathTraceIntegrator.py
@@ -1,6 +1,7 @@
 from Algebra import RGBColour
 from Algebra import BLACK, Vector3D, Cross, Normalize, Length, Dot
 from objetos import Objeto, Light, ObjectQuadric
+from math import pow
 
 
 class PathTraceIntegrator:
@@ -44,6 +45,23 @@ class PathTraceIntegrator:
                     pass
 
                 lv = 1.0 * float (obj.kd) * Dot(p1, p2)
+
+                result = result + (RGBColour(lv, lv, lv))
+
+                # Iluminação especular
+                p1 = Vector3D(p1.x * (-1), p1.y, p1.z)
+                from main import eye
+                p2 = eye
+
+                if (Length(p1)!= 1) :
+                    p1 = Normalize(p1)
+                    pass
+
+                if (Length(eye)!= 1) :
+                    p2 = Normalize(eye)
+                    pass
+
+                lv = 1.0 * float (obj.ks) * pow(Dot(p1, p2), float (obj.n))
 
                 result = result + (RGBColour(lv, lv, lv))
 
