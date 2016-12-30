04557e6b585e813d9fb253c6d0eb44b9e65da033
luisdelgado <lhds@cin.ufpe.br>
2016-12-30 03:20:36 -0300

    Raio primário

--- PhotonMappingIntegrator.py
+++ PhotonMappingIntegrator.py
@@ -1,14 +1,20 @@
 from Algebra import Vector3D
+from math import sqrt, cos, pi
+from random import uniform
+import Photon
 
 class PhotonMappingIntegrator:
-    numberPhotons = 1
+    numeroPhotons = 2.0
+    contador = 0
+    depthOriginal = 4
 
     # Initializer - creates object list
     def __init__(self):
         self.obj_list = []
+        self.photons_list = []
 
     #photon path
-    def trace_ray(self, ray, depth):
+    def photon_ray(self, ray, depth):
 
         # Checando interseções com cada objeto
         dist = 100
@@ -17,22 +23,35 @@ class PhotonMappingIntegrator:
         hit_point = Vector3D(0.0, 0.0, 0.0)
         normal = Vector3D(0.0, 0.0, 0.0)
 
-        for obj in self.obj_list:
-            inter = obj.intersect(ray)
-            tmp_hit = inter[0]
-            distance = inter[1]
+        for n in self.numeroPhotons :
 
-            if tmp_hit and distance < dist:
-                dist = distance
-                objeto = obj
-                hit = tmp_hit
-                hit_point = inter[2]
-                normal = inter[3]
+            for obj in self.obj_list:
 
-        if hit: ## Se o raio bateu no objeto calculamos a cor do ponto
-            if isinstance(objeto, Light):
-                return objeto.color
-            else:
-                result = local_color(objeto, normal, ray, self.ambient)
-        else:
-            return self.background
+                # setado com os valores da fonte de luz para o primeiro caso
+                if self.depth == 4 :
+                    inter = obj.intersect(-0.9100, 3.8360 -23.3240), (0.0, -1.0, 0.0)
+                else:
+                    inter = obj.intersect(ray)
+
+                tmp_hit = inter[0]
+                distance = inter[1]
+
+                if tmp_hit and distance < dist:
+                    dist = distance
+                    objeto = obj
+                    hit = tmp_hit
+                    hit_point = inter[2]
+                    normal = inter[3]
+
+            if hit: ## Se o raio bateu no objeto adicionamos o photon a estrutura de dados
+                R1 = uniform(0.0, 1.0)
+                R2 = uniform(0.0, 1.0)
+                direcaoPhi = 1/cos(sqrt(R1))
+                direcaoTheta = 2*pi*R2
+                R = 1.0
+                T = 1.0
+                self.photons_list[self.contador] = Photon(hit_point, R*1.0/self.numeroPhotons, T*1.0/self.numeroPhotons, direcaoPhi, direcaoTheta)
+                self.contador = self.contador +1
+
+                # chamar raio secundário
+                # http://www.cs.cmu.edu/afs/cs/academic/class/15462-s12/www/lec_slides/lec18.pdf
